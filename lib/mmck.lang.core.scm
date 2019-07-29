7f404e03826a8caa2ccbb7663ad41614632ccdb1
Marco Maggi <marco.maggi-ipsu@poste.it>
2019-07-29 08:28:34 +0200

    development of core syntaxes

--- lib/mmck.lang.core.scm
+++ lib/mmck.lang.core.scm
@@ -30,14 +30,192 @@
 	 (emit-import-library mmck.lang.core))
 
 (module (mmck.lang.core)
-    (the-func)
-  (import (scheme))
+    ((syntax: define-syntax-rule)
+     (syntax: define*)
+     (syntax: case-define)
+     (syntax: case-define*)
+     (syntax: alist-var-cons! cons)
+     (syntax: receive-and-return apply values call-with-values)
+     (syntax: begin0)
+     (syntax: named-lambda)
+     (syntax: internal-body)
+     (syntax: define-auxiliary-syntaxes))
+  (import (scheme)
+          (only (chicken module)
+		reexport))
+  (reexport (only (chicken base)
+		  case-lambda
+		  receive))
+  (import-for-syntax (scheme)
+		     (only (chicken syntax)
+			   ir-macro-transformer
+			   er-macro-transformer
+			   syntax-error)
+		     (only (matchable)
+			   match))
 
 
-;;;; miscellaneous functions
+;;;; binding syntaxes
 
-(define (the-func)
-  #t)
+(define-syntax define-syntax-rule
+  (syntax-rules ()
+    ((_ (?name . ?args) ?body0 ?body ...)
+     (define-syntax ?name
+       (syntax-rules ()
+	 ((_ . ?args)
+	  (begin ?body0 ?body ...)))))
+    ))
+
+(define-syntax case-define
+  (syntax-rules ()
+    ((_ ?who (?formals_ ?body0_ ?body_ ...) (?formals ?body0 ?body ...) ...)
+     (define ?who
+       (case-lambda (?formals_ ?body0_ ?body_ ...)
+		    (?formals  ?body0  ?body  ...)
+		    ...)))
+    ))
+
+(define-syntax case-define*
+  (er-macro-transformer
+    (lambda (input-form.stx rename compare)
+      (define %case-lambda	(rename 'case-lambda))
+      (define %define		(rename 'define))
+      (define %let-syntax	(rename 'let-syntax))
+      (define %quote		(rename 'quote))
+      (define %syntax-rules	(rename 'syntax-rules))
+      (define (synner message . args)
+	(apply syntax-error 'case-define* message input-form.stx args))
+      (match input-form.stx
+	((_ ?who (?formals* ?body0* ?body** ...) ...)
+	 (let ((clauses.stx (map (lambda (formals.stx body0.stx body*.stx)
+				   `(,formals.stx
+				     (,%let-syntax ((__who__ (,%syntax-rules () ((_) (,%quote ,?who)))))
+						   ,body0.stx . ,body*.stx)))
+			      ?formals* ?body0* ?body**)))
+	   `(,%define ,?who (,%case-lambda . ,clauses.stx))))
+	(_
+	 (synner "invalid syntax use"))))))
+
+(define-syntax define-auxiliary-syntaxes
+  (syntax-rules ()
+    ((_ ?name0)
+     (define-syntax ?name0 (syntax-rules ())))
+    ((_ ?name0 ?name1 ?name ...)
+     (begin
+       (define-auxiliary-syntaxes ?name0)
+       (define-auxiliary-syntaxes ?name1 ?name ...)))
+    ))
+
+;;; --------------------------------------------------------------------
+
+(define-syntax define*
+  (ir-macro-transformer
+    (lambda (input-form.stx inject compare)
+      (match input-form.stx
+	((_ (?who . ?formals) ?body0 ?body* ...)
+	 (let ((%__who__ (inject '__who__)))
+	   `(define (,?who . ,?formals)
+	      (let-syntax ((,%__who__ (syntax-rules ()
+					((_)
+					 (quote ,?who)))))
+		,?body0 ,@?body*))))
+
+	((_ ?who ?expr)
+	 (let ((%__who__ (inject '__who__)))
+	   `(define ,?who
+	      (let-syntax ((,%__who__ (syntax-rules ()
+					((_)
+					 (quote ,?who)))))
+		,?expr))))
+	))))
+
+;;; --------------------------------------------------------------------
+
+(define-syntax receive-and-return
+  (ir-macro-transformer
+    (lambda (input-form.stx inject compare)
+      (define (synner message . args)
+	(apply syntax-error 'receive-and-return message input-form.stx args))
+
+      (match input-form.stx
+	((_ () ?expr ?body0 ?body* ...)
+	 `(begin
+	    (call-with-values
+		(lambda () ,?expr)
+	      void)
+	    ,?body0 . ,?body*))
+
+	((_ ((? symbol? ?single-formal)) ?expr ?body0 ?body* ...)
+	 `(let ((,?single-formal ,?expr))
+	    (begin ,?body0 ,@?body*)
+	    ,?single-formal))
+
+	((_ (?formal0 ?formal* ...) ?expr ?body0 ?body* ...)
+	 `(call-with-values
+	      (lambda () ,?expr)
+	    (lambda (,?formal0 ,@?formal*)
+	      (begin ,?body0 ,@?body*)
+	      (values ,?formal0 ,@?formal*))))
+
+	((_ (?formal0 . ?formals-rest) ?expr ?body0 ?body* ...)
+	 `(call-with-values
+	      (lambda () ,?expr)
+	    (lambda (,?formal0 ,@?formal* ...)
+	      (begin ,?body0 ,@?body)
+	      (values ,?formal0 . ,?formals-rest))))
+
+	((_ (? symbol? ?formals-args) ?expr ?body0 ?body* ...)
+	 `(call-with-values
+	      (lambda () ,?expr)
+	    (lambda ,?formals-args
+	      (begin ,?body0 ,@?body*)
+	      (apply values ,?formals-args))))
+
+	(_
+	 (synner "invalid syntax"))))))
+
+
+;;;; expression syntaxes
+
+(define-syntax __who__
+  (er-macro-transformer
+    (lambda (input-form.stx rename compare)
+      (syntax-error '__who__ "invalid syntax use of \"__who__\" outside of a binding form" input-form.stx))))
+
+(define-syntax alist-var-cons!
+  (syntax-rules ()
+    ((_ ?alist-var ?key ?val)
+     (set! ?alist-var (cons (cons ?key ?val) ?alist-var)))
+    ))
+
+(define-syntax internal-body
+  (syntax-rules ()
+    ((_ ?body0 ?body ...)
+     (let () ?body0 ?body ...))
+    ))
+
+(define-syntax begin0
+  (syntax-rules ()
+    ((_ ?form0)
+     (begin ?form0))
+    ((_ ?form0 ?form1 ?form ...)
+     (receive-and-return args
+	 ?form0
+       (begin ?form1 ?form ...)))
+    ))
+
+(define-syntax named-lambda
+  (ir-macro-transformer
+    (lambda (input-form.stx inject compare)
+      (match input-form.stx
+	((_ (? symbol? ?who) ?formals ?body0 ?body* ...)
+	 (let ((%__who__	(inject '__who__)))
+	   `(lambda ,?formals
+	      (let-syntax ((,%__who__ (syntax-rules ()
+					((_)
+					 (quote ,?who)))))
+		,?body0 ,@?body*))))
+	))))
 
 
 ;;;; done
--- tests/test-lang-core.scm
+++ tests/test-lang-core.scm
@@ -26,23 +26,368 @@
 
 ;;;; units and module header
 
-(module (test-core)
+(module (test-lang-core)
     ()
   (import (scheme)
+	  (prefix (chicken condition)
+		  chicken::)
 	  (mmck lang)
+	  (mmck exceptional-conditions)
 	  (mmck checks))
+  (import-for-syntax (scheme)
+		     (only (chicken syntax)
+			   ir-macro-transformer
+			   er-macro-transformer
+			   syntax-error)
+		     (only (matchable)
+			   match))
 
 (check-set-mode! 'report-failed)
 (check-display "*** testing language extensions: core syntaxes\n")
 
 
-(parameterise ((check-test-name		'alpha))
+(parameterise ((check-test-name		'define*))
 
   (check
-      (the-func)
-    => #t)
+      (let ()
+	(define* (doit a b)
+	  (list (__who__) a b))
+
+	(doit 'A 'B))
+    => '(doit A B))
+
+  (check
+      (let ()
+	(define* (doit a b . rest)
+	  (list (__who__) a b rest))
+
+	(doit 'A 'B 'C 'D 'E))
+    => '(doit A B (C D E)))
+
+  (check
+      (let ()
+	(define* (doit . args)
+	  (list (__who__) args))
+
+	(doit 'A 'B 'C 'D 'E))
+    => '(doit (A B C D E)))
+
+;;; --------------------------------------------------------------------
+;;; defining variables
+
+  (check
+      (let ()
+	(define* it
+	  (list (__who__) 1 2))
+
+	it)
+    => '(it 1 2))
+
+;;; --------------------------------------------------------------------
+;;; raising exceptions
+
+  (check
+      (let ()
+	(define* (raise-it)
+	  (chicken::abort
+	   (chicken::make-property-condition 'mine 'location (__who__))))
+
+	(chicken::condition-case (raise-it)
+				 (E (mine)
+				    (chicken::get-condition-property E 'mine 'location))
+				 (()
+				  #f)))
+    => 'raise-it)
+
+  (check
+      (let ()
+	(define* (raise-it)
+	  (raise
+	   (condition (make-error)
+		      (make-who-condition (__who__)))))
+
+	(guard (E ((error? E)
+		   (condition-who E))
+		  (else #f))
+	  (raise-it)))
+    => 'raise-it)
 
   (values))
+
+
+(parameterise ((check-test-name		'case-define))
+
+  (check
+      (let ()
+	(case-define doit
+	  (()			0)
+	  ((a)			(list 1 a))
+	  ((a b)		(list 2 a b))
+	  ((a b c . rest)	(list '* a b c rest)))
+
+	(values (doit)
+		(doit 'A)
+		(doit 'A 'B)
+		(doit 'A 'B 'C 'D 'E)))
+    => 0 '(1 A) '(2 A B) '(* A B C (D E)))
+
+  (check
+      (let ()
+	(case-define doit
+	  (()		0)
+	  (args		(list '* args)))
+
+	(values (doit)
+		(doit 'A 'B 'C 'D 'E)))
+    => 0 '(* (A B C D E)))
+
+  (values))
+
+
+(parameterise ((check-test-name		'case-define*))
+
+  (check
+      (let ()
+	(case-define* doit
+	  (()			0)
+	  ((a)			(list 1 a))
+	  ((a b)		(list 2 a b))
+	  ((a b c . rest)	(list '* a b c rest)))
+
+	(values (doit)
+		(doit 'A)
+		(doit 'A 'B)
+		(doit 'A 'B 'C 'D 'E)))
+    => 0 '(1 A) '(2 A B) '(* A B C (D E)))
+
+  (check
+      (let ()
+	(case-define* doit
+	  (()		0)
+	  (args		(list '* args)))
+
+	(values (doit)
+		(doit 'A 'B 'C 'D 'E)))
+    => 0 '(* (A B C D E)))
+
+  ;;Test for the "__who__" syntax.
+  ;;
+  (check
+      (let ()
+	(case-define* doit
+	  (()			(list (__who__) 0))
+	  ((a)			(list (__who__) 1 a))
+	  ((a b)		(list (__who__) 2 a b))
+	  ((a b c . rest)	(list (__who__) '* a b c rest)))
+
+	(values (doit)
+		(doit 'A)
+		(doit 'A 'B)
+		(doit 'A 'B 'C 'D 'E)))
+    => '(doit 0) '(doit 1 A) '(doit 2 A B) '(doit * A B C (D E)))
+
+  (values))
+
+
+(parameterise ((check-test-name		'receive-and-return))
+
+  (check
+      (receive (a b c)
+	  (receive-and-return (a b c)
+	      (values 1 2 3)
+	    (vector a b c))
+	(list a b c))
+    => '(1 2 3))
+
+  (check
+      (with-result
+	(receive (a)
+	    (receive-and-return (a)
+		1
+	      (add-result a))
+	  a))
+    => '(1 (1)))
+
+  (check
+      (with-result
+	(receive-and-return ()
+	    (values)
+	  (add-result 1))
+	#t)
+    => '(#t (1)))
+
+  (values))
+
+
+(parameterise ((check-test-name		'define-auxiliary-syntaxes))
+
+  (check
+      (let ()
+	(define-auxiliary-syntaxes ciao)
+
+	(define-syntax doit
+	  (syntax-rules (ciao)
+	    ((_ ciao)
+	     #t)
+	    ((_ ?thing)
+	     #f)))
+
+	(values (doit ciao)
+		(doit hello)))
+    => #t #f)
+
+  (check
+      (let ()
+	(define-auxiliary-syntaxes ciao)
+
+	(define-syntax doit
+	  (er-macro-transformer
+	    (lambda (input-form.stx rename compare)
+	      (define %ciao (rename 'ciao))
+	      (define (ciao? obj)
+		(compare %ciao obj))
+
+	      (match input-form.stx
+		((_ (? ciao?))
+		 #t)
+		(_
+		 #f)))))
+
+	(values (doit ciao)
+		(doit hello)))
+    => #t #f)
+
+  (check
+      (let ()
+	(define-auxiliary-syntaxes ciao)
+
+	(define-syntax doit
+	  (ir-macro-transformer
+	    (lambda (input-form.stx inject compare)
+	      (define %ciao (inject 'ciao))
+	      (define (ciao? obj)
+		(compare %ciao obj))
+
+	      (match input-form.stx
+		((_ (? ciao?))
+		 #t)
+		(_
+		 #f)))))
+
+	(values (doit ciao)
+		(doit hello)))
+    => #t #f)
+
+  (values))
+
+
+(parameterise ((check-test-name		'define-syntax-rule))
+
+  (check
+      (let ()
+	(define-syntax-rule (doit a b)
+	  (list a b))
+
+	(doit 1 2))
+    => '(1 2))
+
+  (check
+      (let ()
+	(define-syntax-rule (doit)
+	  (list 1 2))
+
+	(doit))
+    => '(1 2))
+
+  (check
+      (let ()
+	(define-syntax-rule (doit ?a ?b . ?rest)
+	  (vector '?a '?b '?rest))
+
+	(doit 1 2 3 4))
+    => '#(1 2 (3 4)))
+
+  (values))
+
+
+(parameterise ((check-test-name	'begin0))
+
+  (check
+      (begin0
+	  1)
+    => 1)
+
+  (check
+      (call-with-values
+	  (lambda ()
+	    (begin0
+		(values 1 2 3)))
+	list)
+    => '(1 2 3))
+
+  (check
+      (with-result
+	(begin0
+	    1
+	  (add-result 2)
+	  (add-result 3)))
+    => '(1 (2 3)))
+
+  (check
+      (with-result
+	(call-with-values
+	    (lambda ()
+	      (begin0
+		  (values 1 10)
+		(add-result 2)
+		(add-result 3)))
+	  list))
+    => '((1 10) (2 3)))
+
+  #t)
+
+
+(parameterise ((check-test-name		'named-lambda))
+
+  (check
+      (let ()
+	(define func
+	  (named-lambda the-func (a b)
+	    (list (__who__) a b)))
+
+	(func 1 2))
+    => '(the-func 1 2))
+
+  (values))
+
+
+(parameterise ((check-test-name	'internal-body))
+
+  (check
+      (with-result
+	(let ()
+	  (define a 2)
+	  (add-result 1)
+	  (add-result a)
+	  (internal-body
+	    (define b 4)
+	    (define c 5)
+	    (add-result b)
+	    (add-result c)
+	    (+ a b c))))
+    => `(,(+ 2 4 5) (1 2 4 5)))
+
+  (check
+      (with-result
+	(internal-body
+	  (define a 1)
+	  (define b 2)
+	  (add-result a)
+	  (add-result b)
+	  (+ a b)))
+    => `(,(+ 1 2) (1 2)))
+
+  #t)
 
 
 ;;;; done
